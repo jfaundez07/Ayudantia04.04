3baa84f7652eadde726ea5372866adfa73e178de
jfaundez07 <j.faundez07@ufromail.cl>
2024-04-03 14:54:21 -0300

    EStructura basica de metodos

---
+++ src/main/java/LecturaDatos.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LecturaDatos {
+
+    public static Scanner leer = new Scanner(System.in);
+
+    public static int leerEntero() {
+        return leer.nextInt();
+    }
+
+    public static String leerString() {
+        return leer.next();
+    }
+
+    public static int stringToIng(String texto) {
+        return Integer.parseInt(texto);
+    }
+
+}
