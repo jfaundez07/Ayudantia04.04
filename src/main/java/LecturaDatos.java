a2310b77b3fedbbf304e22f4af23262f669ee654
jfaundez07 <j.faundez07@ufromail.cl>
2024-04-04 18:55:07 -0300

    Solicitud de dato con bloque do-while

--- src/main/java/LecturaDatos.java
+++ src/main/java/LecturaDatos.java
@@ -4,37 +4,36 @@ import java.util.Scanner;
 public class LecturaDatos {
 
     public static void main(String[] args) {
-        stringToIng("a");
-        System.out.println("no se cayo");
+        int dato = solicitarDato();
+        System.out.println("El número introducido es: " + dato);
     }
 
     public static Scanner leer = new Scanner(System.in);
 
-    public static int leerEntero() {
-        try{
-            //lo que yo espero que haga
-            return leer.nextInt();
-        }catch (InputMismatchException e) {
-            //lo que debe ocurrir en caso de que falle
-            System.out.println("Tipo de dato incorrecto");
-        } //expecion que quier encapsular
+    public static int solicitarDato(){
 
-        return 0;
+        int dato = 0; //inicializamos una variable que almacena el numero
+        boolean continuar = true; //inicializamos una variable para mantener y terminar
+                                // el bucle do-while
+
+        do{
+
+            try{
+
+                System.out.println("Introduce un número entero: ");
+                dato = leer.nextInt(); //leemos el número entero
+                continuar = false; //si no hay excepción, cambiamos el valor de continuar
+
+            }catch(InputMismatchException e){
+
+                System.out.println("Error: Debes introducir un número entero.");
+                System.out.println("Exception: " + e);
+                leer.nextLine();//limpia el espacio de memoria donde esta ubicada la excepción
+                                // (dato ingresado incorrectamente)
+            }
+
+        }while(continuar);
+
+        return dato;
     }
-
-    public static String leerString() { //no requiere manejo de excepcion.
-        return leer.next();
-    }
-
-   public static int stringToIng(String texto) {
-        try {
-            return Integer.parseInt(texto);
-        } catch (NumberFormatException e) {
-            System.out.println("Tipo de dato incorrecto, expecion: " + e.getMessage());
-        }
-        return 0;
-    }
-    // NumberFormatException
-
-
 }
