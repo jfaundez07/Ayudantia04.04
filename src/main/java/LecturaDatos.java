bbe2f1441dfcc5ee529f7ea6eec001dac2a0c2bc
jfaundez07 <j.faundez07@ufromail.cl>
2024-04-04 09:54:59 -0300

    metpdo leer int

--- src/main/java/LecturaDatos.java
+++ src/main/java/LecturaDatos.java
@@ -1,19 +1,34 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LecturaDatos {
 
+    public static void main(String[] args) {
+        System.out.println("Ingrese un entero: ");
+        int entero = leerEntero();
+        System.out.println("No se cayo el programa");
+    }
+
     public static Scanner leer = new Scanner(System.in);
 
     public static int leerEntero() {
-        return leer.nextInt();
+        try{
+            //lo que yo espero que haga
+            return leer.nextInt();
+        }catch (InputMismatchException e) {
+            //lo que debe ocurrir en caso de que falle
+            System.out.println("Tipo de dato incorrecto");
+        } //expecion que quier encapsular
+
+        return 0;
     }
 
     public static String leerString() {
         return leer.next();
     }
 
-    public static int stringToIng(String texto) {
-        return //string en entero;
-    }
+   // public static int stringToIng(String texto) {
+     //   return //string en entero;
+    //}
 
 }
